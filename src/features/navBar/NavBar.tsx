7253f0f68761de7e83fc5142318a40fa766af5c9
Dan Benson <dbenson3091@gmail.com>
2024-04-17 21:37:43 -0500

    updated nav section with clerk for logging out

--- src/features/navBar/NavBar.tsx
+++ src/features/navBar/NavBar.tsx
@@ -1,30 +1,24 @@
 import Link from 'next/link';
 
-// import { useState } from "react";
-// import { useSelector, useDispatch } from "react-redux";
-// import { Link, useNavigate } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
-// import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
 import StorefrontIcon from "@mui/icons-material/Storefront";
+import { LockOpen } from "@mui/icons-material";
 import {
   Box,
   Container,
   AppBar,
   Toolbar,
-//   Tooltip,
   Typography,
   Button,
-//   Menu,
   IconButton,
-//   Avatar,
-//   MenuItem,
 } from "@mui/material";
-// import { setAuthUser, setToken } from "../../appSlice";
+
 import useToggle from "../../hooks/useToggle";
 import SideModal from '~/components/SideModal';
 import ListItems from "./components/ListItem";
-import { LockOpen } from "@mui/icons-material";
-// import { useLogoutCurentUserMutation } from "../../service/authService";
+
+import { useUser, UserButton } from '@clerk/nextjs';
 
 const navItems = [
   {
@@ -34,15 +28,13 @@ const navItems = [
   },
 ];
 
-// const navItemsAuth = [
-//   {
-//     title: "Recipes",
-//     link: "/recipes",
-//     icon: <RestaurantMenuIcon />,
-//   },
-// ];
-
-// const navUserSettings = ["Logout"];
+const navItemsAuth = [
+  {
+    title: "Recipes",
+    link: "/recipes",
+    icon: <RestaurantMenuIcon />,
+  },
+];
 
 function MobileNavBar({
   navItems,
@@ -80,42 +72,19 @@ function MobileNavBar({
       >
         <MenuIcon />
       </IconButton>
-      {/* <SideModal modalOpen={value} handleToggle={() => toggleValue()}>
+      <SideModal modalOpen={value} handleToggle={() => toggleValue()}>
         <ListItems handleToggle={() => toggleValue()} listItems={navItems} />
-      </SideModal> */}
+      </SideModal>
     </>
   );
 }
 
 export default function NavBar() {
-//   const user = useSelector((state: any) => state.app.authUser as any) as any;
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
-//   const [logoutCurrentUser] = useLogoutCurentUserMutation();
+  const { isSignedIn, user, isLoaded } = useUser();
 
-//   const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-//     setAnchorElUser(event.currentTarget);
-//   };
+  console.log(isSignedIn, user, isLoaded, "<=== nav checking clerk");
 
-//   const handleCloseUserMenu = (
-//     evt: React.MouseEvent<HTMLElement, MouseEvent>,
-//   ) => {
-//     const target = evt.target as HTMLLIElement;
-
-//     if (target && target.innerText.toLowerCase() === "logout") {
-// 		console.log('event clicked for logged out');
-//     //   logoutCurrentUser();
-//     //   dispatch(setToken(null));
-//     //   dispatch(setAuthUser(null));
-//     //   navigate("/");
-//     }
-
-//     setAnchorElUser(null);
-//   };
-
-  //const navItemsDisplayed = user !== null ? navItemsAuth : navItems;
-  const navItemsDisplayed = navItems;
+  const navItemsDisplayed = user !== null ? navItemsAuth : navItems;
 
   return (
     <AppBar position="static" sx={{ backgroundColor: "#04b597" }}>
@@ -152,30 +121,11 @@ export default function NavBar() {
             </Button>
           </Link>))}
       </Box>
-          {/* {user && (
+          {user && (
             <Box sx={{ display: "flex", flexGrow: { xs: 1, sm: 0 } }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu}>
-                  <Avatar
-                    alt="foodie-user"
-                    src={user && user?.imageUrl ? user.imageUrl : ""}
-                  />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "4px" }}
-                anchorEl={anchorElUser}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {navUserSettings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+              <UserButton/>
             </Box>
-          )} */}
+          )}
           <MobileNavBar navItems={navItemsDisplayed} />
         </Toolbar>
       </Container>
