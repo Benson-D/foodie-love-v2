de9a1ae258cd0b0335bee2d72b31654ecdc1465f
Dan Benson <dbenson3091@gmail.com>
2024-05-19 20:33:35 -0500

    added new variables for s3 buckets

--- src/env.js
+++ src/env.js
@@ -17,6 +17,10 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    BUCKET: z.string(),
+    BUCKET_REGION: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string()
   },
 
   /**
@@ -35,7 +39,10 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    BUCKET: process.env.BUCKET,
+    BUCKET_REGION: process.env.BUCKET_REGION,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
