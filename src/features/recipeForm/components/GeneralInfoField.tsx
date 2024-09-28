8cd9659fd522ad02b4dd6831638bdf5b9a913008
Dan Benson <danmarcbenson@gmail.com>
2024-09-27 22:21:58 -0500

    added alert message for when successful upload has completed

--- src/features/recipeForm/components/GeneralInfoField.tsx
+++ src/features/recipeForm/components/GeneralInfoField.tsx
@@ -1,9 +1,10 @@
 import { UploadButton } from "~/utils/uploadthing";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import FormContext from "../context/FormContext";
 import InputField from "~/components/formFields/InputField";
-import { Box, Grid, Typography, InputAdornment } from "@mui/material";
+import { Box, Grid, Typography, InputAdornment, Alert } from "@mui/material";
+import useToggle from "~/hooks/useToggle";
 
 /**
  * First page of Foodie Recipe Form
@@ -19,7 +20,9 @@ export default function GeneralInfoField({
   handleFile: (file: string | null) => void;
 }) {
   const foodie = useContext(FormContext);
-
+  const [alertSuccessValue, toggleAlertSuccess] = useToggle(false);
+  const [alertErrorValue, toggleAlertError] = useToggle(false);
+  
   const componentDisplay = foodie?.step === 0 ? "block" : "none";
   const minuteProps = {
     endAdornment: <InputAdornment position="end">Minutes</InputAdornment>,
@@ -62,13 +65,25 @@ export default function GeneralInfoField({
                 handleFile(res[0].url);
               }
 
-              alert("Upload Completed");
+              toggleAlertSuccess(true);
             }}
             onUploadError={(error: Error) => {
-              // Do something with the error.
-              alert(`ERROR! ${error.message}`);
+              console.error(error);
+              toggleAlertError(true);
             }}
           />
+        </Grid>
+        <Grid item xs={12} sm={12} md={6}>
+          {alertSuccessValue && 
+            <Alert severity="success" sx={{ padding: "3px 16px" }}>
+              Upload Successful!
+            </Alert>
+          }
+          {alertErrorValue && 
+            <Alert severity="error" sx={{ padding: "3px 16px" }}>
+              Upload Unsuccessful
+            </Alert>
+          }
         </Grid>
       </Grid>
     </Box>
